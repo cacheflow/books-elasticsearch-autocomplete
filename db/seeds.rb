68ea3a8875afe67192d370ea4d2df24fa5eaf19c
Lex Alexander <l.alexander10@gmail.com>
2015-04-22 09:11:59 -0700

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(name: "lex", email: "tim@tim.com")
+User.create(name: "jim alexander", email: "lenis@lenis.com")
+User.create(name: "jim alexander", email: "lenis@lenis.com")
+User.create(name: "jim alexander", email: "lenis@lenis.com")
+User.create(name: "jim alexander", email: "lenis@lenis.com")
+User.create(name: "jim alexander", email: "lenis@lenis.com")
+User.create(name: "jim alexander", email: "lenis@lenis.com")
+User.create(name: "foo", email: "aol@aol.com")
+User.create(name: "foo", email: "aol@aol.com")
+User.create(name: "foo", email: "aol@aol.com")
+User.create(name: "foo", email: "aol@aol.com")
+User.create(name: "foo", email: "aol@aol.com")
+User.create(name: "foo", email: "aol@aol.com")
+User.create(name: "dom", email: "dom@dom.com")
+User.create(name: "dom", email: "dom@dom.com")
+User.create(name: "dom", email: "dom@dom.com")
+User.create(name: "dom", email: "dom@dom.com")
+User.create(name: "dom", email: "dom@dom.com")
+User.create(name: "dom", email: "dom@dom.com")
